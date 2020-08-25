5ee2f9517bdd42598efcc6ef6555cfd0d3bcaa73
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-08-25 23:00:10 +0430

    handle IllegalArgumentException and response code

---
+++ src/main/java/com/example/communicationservice/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.example.communicationservice.exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.badRequest().build();
+    }
+
+}
