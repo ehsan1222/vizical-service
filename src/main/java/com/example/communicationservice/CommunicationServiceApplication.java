327c1de409d310c33fc71ad51b282c06389ad0a8
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-08-26 14:36:15 +0430

    add some users

--- src/main/java/com/example/communicationservice/CommunicationServiceApplication.java
+++ src/main/java/com/example/communicationservice/CommunicationServiceApplication.java
@@ -1,7 +1,17 @@
 package com.example.communicationservice;
 
+import com.example.communicationservice.models.Roles;
+import com.example.communicationservice.services.UserService;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class CommunicationServiceApplication {
@@ -10,4 +20,15 @@ public class CommunicationServiceApplication {
 		SpringApplication.run(CommunicationServiceApplication.class, args);
 	}
 
+	@Autowired
+	private UserService userService;
+
+	@Bean
+	public InitializingBean initializingBean() {
+		return () -> {
+			userService.addUser("user", "password", Roles.USER);
+			userService.addUser("admin", "admin", Roles.ADMIN);
+
+		};
+	}
 }
