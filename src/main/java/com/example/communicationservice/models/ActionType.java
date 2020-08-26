a1cce7dff04d0ec3cdcc0341d4d15bfca767adb1
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-08-26 14:37:03 +0430

    chane ActionType enum

--- src/main/java/com/example/communicationservice/models/ActionType.java
+++ src/main/java/com/example/communicationservice/models/ActionType.java
@@ -1,5 +1,5 @@
 package com.example.communicationservice.models;
 
 public enum ActionType {
-    FILE_UPLOAD, FILE_DOWNLOAD
+    JAR_FILE_UPLOAD, TXT_FILE_UPLOAD, FILE_DOWNLOAD, FILE_SHOW, CHECK_FILE_EXIST
 }
