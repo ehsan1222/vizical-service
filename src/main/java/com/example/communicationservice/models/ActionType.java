b8f777140279369309094dc5765a47773a740e20
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-08-30 00:43:09 +0430

    change ActionType enum values

--- src/main/java/com/example/communicationservice/models/ActionType.java
+++ src/main/java/com/example/communicationservice/models/ActionType.java
@@ -1,5 +1,5 @@
 package com.example.communicationservice.models;
 
 public enum ActionType {
-    JAR_FILE_UPLOAD, TXT_FILE_UPLOAD, FILE_DOWNLOAD, FILE_SHOW, CHECK_FILE_EXIST
+    JAR_FILE_UPLOAD, MXE_FILE_UPLOAD, FILE_DOWNLOAD, FILE_SHOW, CHECK_FILE_EXIST
 }
