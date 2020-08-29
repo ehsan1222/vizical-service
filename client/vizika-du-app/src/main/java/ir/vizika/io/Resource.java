755d925ac06f1ce6d5445d6d7e372e0481eefa1b
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-08-30 01:37:45 +0430

    implements authentication header in requests

---
+++ client/vizika-du-app/src/main/java/ir/vizika/io/Resource.java
@@ -0,0 +1,56 @@
+package ir.vizika.io;
+
+import org.json.JSONArray;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Resource {
+
+    String baseDownloadPath = new File(".").getAbsolutePath() + File.separator + "downloads";
+
+    public boolean storeFile(InputStream content, String fileName) {
+        File baseDirectory = new File(baseDownloadPath);
+
+        if (!baseDirectory.isDirectory()) {
+            baseDirectory.mkdirs();
+        }
+
+        String path = baseDirectory.getAbsolutePath().concat(File.separator).concat(fileName);
+
+        System.out.println(baseDownloadPath);
+        System.out.println(path);
+        try {
+            Files.write(Paths.get(path), content.readAllBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+//            return false;
+        }
+        return true;
+    }
+
+    public List<String> convertInputStreamToList(InputStream inputStream) {
+        String jsonString = convertStreamToString(inputStream);
+        JSONArray jsonArray = convertStringToJsonObject(jsonString);
+        List<String> files = new ArrayList<>();
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            files.add(jsonArray.getString(i));
+        }
+        return files;
+    }
+
+    private String convertStreamToString(InputStream inputStream) {
+        return new BufferedReader(new InputStreamReader(inputStream))
+                .lines().collect(Collectors.joining("\n"));
+    }
+
+    private JSONArray convertStringToJsonObject(String json) {
+        return new JSONArray(json);
+    }
+
+}
---
+++ client/vizika-du-app/src/main/java/ir/vizika/net/ServerProcesses.java
@@ -0,0 +1,147 @@
+package ir.vizika.net;
+
+import ir.vizika.io.Resource;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+import java.util.Scanner;
+
+public class ServerProcesses {
+
+    public static final String HEADER_AUTHENTICATE = "Authorization";
+    public static final String AUTHENTICATE_PREFIX = "Basic ";
+    private final String baseUrl = "http://localhost:8080/files";
+
+    public List<String> listOfFileNames() {
+        List<String> result = new ArrayList<>();
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpGet get = new HttpGet(baseUrl);
+            String encodedAuthenticate = encodeAuthenticateHeader("user", "password");
+            get.addHeader(HEADER_AUTHENTICATE, AUTHENTICATE_PREFIX + encodedAuthenticate);
+            try (CloseableHttpResponse response = httpClient.execute(get)) {
+                if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                    InputStream content = response.getEntity().getContent();
+
+                    Resource resource = new Resource();
+                    result = resource.convertInputStreamToList(content);
+                    return result;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public boolean uploadFile(File file, String urlPath) {
+        String url = baseUrl.concat("/").concat(urlPath);
+
+        boolean isFileExisted         = isFileExistInServer(file.getName());
+        boolean continueUploadProcess = true;
+
+        if (isFileExisted) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("file exist on server:");
+            System.out.println("1. Force Upload");
+            System.out.println("2. Cancel Upload");
+            String command = scanner.nextLine();
+            if (command.equals("2")) {
+                continueUploadProcess = false;
+            }
+        }
+
+        if (continueUploadProcess) {
+            try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+                HttpPost post = new HttpPost(url);
+                String encodedAuthenticate = encodeAuthenticateHeader("user", "password");
+                post.addHeader(HEADER_AUTHENTICATE, AUTHENTICATE_PREFIX + encodedAuthenticate);
+
+                FileBody fileBody = new FileBody(file, ContentType.DEFAULT_BINARY);
+
+                MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+                builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+                builder.addPart("file", fileBody);
+
+                HttpEntity entity = builder.build();
+                post.setEntity(entity);
+                try (CloseableHttpResponse response = httpClient.execute(post)) {
+
+                    int statusCode = response.getStatusLine().getStatusCode();
+                    System.out.println(statusCode);
+
+                    if (statusCode == HttpStatus.SC_OK) {
+                        return true;
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return false;
+    }
+
+    public boolean downloadFile(String fileName) {
+        String url = baseUrl.concat("/").concat(fileName);
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpGet get = new HttpGet(url);
+            String encodedAuthenticate = encodeAuthenticateHeader("user", "password");
+            get.addHeader(HEADER_AUTHENTICATE, AUTHENTICATE_PREFIX + encodedAuthenticate);
+            try (CloseableHttpResponse response = httpClient.execute(get)) {
+                int statusCode = response.getStatusLine().getStatusCode();
+                if (statusCode == HttpStatus.SC_OK) {
+                    InputStream content = response.getEntity().getContent();
+                    Resource resource = new Resource();
+                    // store file in downloads directory and if process is ok then true be return otherwise false return
+                    return resource.storeFile(content, fileName);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private boolean isFileExistInServer(String fileName) {
+        // url must be: /file/{fileName}/check
+        String url = baseUrl.concat("/").concat(fileName).concat("/").concat("check");
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpGet get = new HttpGet(url);
+            String encodedAuthenticate = encodeAuthenticateHeader("user", "password");
+            get.addHeader(HEADER_AUTHENTICATE, AUTHENTICATE_PREFIX + encodedAuthenticate);
+            try (CloseableHttpResponse httpResponse = httpClient.execute(get)) {
+                int statusCode = httpResponse.getStatusLine().getStatusCode();
+                if (statusCode == HttpStatus.SC_OK) {
+                    return true;
+                } else if (statusCode == HttpStatus.SC_NO_CONTENT) {
+                    return false;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private String encodeAuthenticateHeader(String username, String password) {
+        String token = username + ":" + password;
+        return Base64.getEncoder().encodeToString(token.getBytes());
+    }
+
+
+}
