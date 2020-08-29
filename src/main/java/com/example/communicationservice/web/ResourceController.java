9cf7847d52868d5f1b07a76f9b0754ac858b11aa
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-08-30 00:50:59 +0430

    update resource controller

--- src/main/java/com/example/communicationservice/web/ResourceController.java
+++ src/main/java/com/example/communicationservice/web/ResourceController.java
@@ -10,12 +10,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping(
-        path = "/file"
+        path = "/files"
 )
 @Log4j2
 public class ResourceController {
@@ -24,24 +23,32 @@ public class ResourceController {
     private StorageService storageService;
 
     @GetMapping
-    public ResponseEntity<List<String>> getListOfFiles() {
-        List<String> files = storageService.listOfFiles();
-        return ResponseEntity.ok(files);
+    public ResponseEntity<List<String>> listOfFiles() {
+        List<String> listOfFiles = storageService.listOfFiles();
+        return new ResponseEntity<>(listOfFiles, HttpStatus.OK);
     }
 
-    @PostMapping("/user/upload")
-    public void textUploadFile(@RequestParam("file") MultipartFile file) {
-        storageService.userUploadFile(file);
+    @PostMapping("/user")
+    public ResponseEntity<?> userUploadFile(@RequestParam("file") MultipartFile file) {
+        storageService.userSaveFile(file);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @PostMapping("/admin/upload")
-    public void jarUploadFile(@RequestParam("file") MultipartFile file) {
-        storageService.adminUploadFile(file);
+    @GetMapping("/admin")
+    public ResponseEntity<List<String>> listOfAdminFiles() {
+        List<String> listOfAdminFiles = storageService.getAdminList();
+        return new ResponseEntity<>(listOfAdminFiles, HttpStatus.OK);
+    }
+
+    @PostMapping("/admin")
+    public ResponseEntity<?> adminUploadFile(@RequestParam("file") MultipartFile file) {
+        storageService.adminSaveFile(file);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping("/{fileName}")
     public ResponseEntity<Resource> downloadFile(@PathVariable("fileName") String fileName) {
-        Resource file = storageService.download(fileName);
+        Resource file = storageService.getFile(fileName);
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
                 .body(file);
@@ -49,11 +56,11 @@ public class ResourceController {
 
     @GetMapping("/{fileName}/check")
     public ResponseEntity<?> checkFileExist(@PathVariable("fileName") String fileName) {
-        boolean checkExist = storageService.isFileExist(fileName);
+        boolean checkExist = storageService.checkFileExist(fileName);
         if (checkExist) {
             return new ResponseEntity<>(HttpStatus.OK);
         }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
