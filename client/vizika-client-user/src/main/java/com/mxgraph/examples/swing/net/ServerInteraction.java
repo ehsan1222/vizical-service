3ac09314a4097c65e7da5bf72a307094e9fc9c2a
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-08-30 11:56:35 +0430

    add authenticate header to requests

--- client/vizika-client-user/src/main/java/com/mxgraph/examples/swing/net/ServerInteraction.java
+++ client/vizika-client-user/src/main/java/com/mxgraph/examples/swing/net/ServerInteraction.java
@@ -2,17 +2,18 @@ package com.mxgraph.examples.swing.net;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 
 @Log4j2
 public class ServerInteraction {
 
+    public static final String AUTHORIZATION_NAME = "Authorization";
+    public static final String AUEHTNTICATE_PREFIX = "Basic ";
     private final String BASE_URL = "http://localhost:8080/files";
 
     public boolean checkFilenameExist(String filename) {
@@ -20,8 +21,14 @@ public class ServerInteraction {
         log.info("start checking file exist....");
 
         RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        String encodedAuthHeader = getEncodedAuthenticationHeader();
+        headers.add(AUTHORIZATION_NAME, AUEHTNTICATE_PREFIX + encodedAuthHeader);
+        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+
         log.info("send request to check filename exist to server");
-        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, null, String.class);
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, httpEntity, String.class);
         log.info("receive check filename exist response from server");
         if (response.getStatusCode() == HttpStatus.OK) {
             log.info("receive checkFilenameExist successfully");
@@ -38,10 +45,16 @@ public class ServerInteraction {
         String url = BASE_URL + "/admin";
         log.info("start getJarFilenames from server process...");
 
+        HttpHeaders headers = new HttpHeaders();
+        String encodedAuthHeader = getEncodedAuthenticationHeader();
+        headers.add(AUTHORIZATION_NAME, AUEHTNTICATE_PREFIX + encodedAuthHeader);
+        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+
+
         RestTemplate restTemplate = new RestTemplate();
         log.info("send get all jar filenames request to server");
         ResponseEntity<List<String>> response =
-                restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
+                restTemplate.exchange(url, HttpMethod.GET, httpEntity, new ParameterizedTypeReference<>() {});
         log.info("receive filenames response from server");
         if (response.getStatusCode() == HttpStatus.OK) {
             log.info("receive filenames successfully");
@@ -57,9 +70,14 @@ public class ServerInteraction {
 
         RestTemplate restTemplate = new RestTemplate();
 
+        HttpHeaders headers = new HttpHeaders();
+        String encodedAuthHeader = getEncodedAuthenticationHeader();
+        headers.add(AUTHORIZATION_NAME, AUEHTNTICATE_PREFIX + encodedAuthHeader);
+        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+
         log.info("send download file request to server");
         ResponseEntity<byte[]> response =
-                restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
+                restTemplate.exchange(url, HttpMethod.GET, httpEntity, new ParameterizedTypeReference<>() {});
         log.info("receive downloaded file response from server");
         if (response.getStatusCode() == HttpStatus.OK) {
             log.info("download file successfully");
@@ -67,6 +85,12 @@ public class ServerInteraction {
         }
         log.warn("error in downloadFile process. code: " + response.getStatusCode());
         return null;
+    }
+
+    private String getEncodedAuthenticationHeader() {
+        String username = "user";
+        String password = "password";
+        return Base64.getEncoder().encodeToString((username + ":" + password).getBytes());
     }
 
 }
